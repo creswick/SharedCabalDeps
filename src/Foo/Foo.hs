5c1f5eb273b847351e8648c98e843fabb8fd940b
Rogan Creswick <creswick@galois.com>
2011-05-07 22:22:48 -0700

    initial revision

---
+++ src/Foo/Foo.hs
@@ -0,0 +1,13 @@
+module Foo.Foo
+    ( fooVersion
+    )
+where
+
+import AI.VersionSpaces
+
+fooVersion :: String
+fooVersion = "Foo v1"
+
+fooBSR :: BSR Set Int Int
+fooBSR = undefined -- this dosen't matter.
+
